a437f31269163f9a20b672b99a8e7dbe7f8726de
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-06-27 12:39:22 +0200

    exceptions docs

--- src/exceptions/ProductAlreadyExistException.java
+++ src/exceptions/ProductAlreadyExistException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Thrown to indicate that a product with the given identifier already exists in the inventory.
+ */
 public class ProductAlreadyExistException extends RuntimeException {
     public ProductAlreadyExistException(String message) {
         super(message);
--- src/exceptions/ProductNotFoundException.java
+++ src/exceptions/ProductNotFoundException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Thrown to indicate that a product was not found in the inventory.
+ */
 public class ProductNotFoundException extends RuntimeException {
   public ProductNotFoundException(String message) {
     super(message);
